deb8cb5162a7ce1c20358b344fc26a251ffe7741
dustopia1203 <nhhieu3021@gmail.com>
2024-03-08 14:44:17 +0700

    feat(project): add node modules and finish DatePicker

--- project-3/DatePicker.js
+++ project-3/DatePicker.js
@@ -1,30 +1,96 @@
-function DatePicker(id) {
-	this.id = id;
-}
+'use strict';
 
-DatePicker.prototype.render = function(date) {
-	const week_days = ['sun','mon','tue','wed','thu','fri','sat'];
-	const tbl = document.createElement('table');
-	tbl.style.width = '100px';
-	tbl.style.border = '1px solid black';
-
-	const tr = tbl.insertRow();
-    for (let j = 0; j < 7; j++) {
-    	const td = tr.insertCell();
-	    td.appendChild(document.createTextNode(week_days[j]));
-	    td.style.border = '1px solid black';
-    }
-
-	for (let i = 0; i < 5; i++) {
-		const tr = tbl.insertRow();
-		for (let j = 1; j < 8; j++) {
-		    const td = tr.insertCell();
-		    td.appendChild(document.createTextNode(7*i+j));
-		    td.style.border = '1px solid black';
-		}
+class DatePicker {
+	constructor(id, callback) {
+		this.id = id;
+		this.callback = callback;
+		this.isSettedDate = false;
 	}
 
-	var elem = document.getElementById(this.id);
-	elem.appendChild(tbl);
-}
+	render(date) {
+		if (!this.isSettedDate) {
+			this.date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+			this.isSettedDate = true;
+		}
+		console.log(this.date);
+		//#create calendar
+		const calendar = document.getElementById(this.id);
+		//#add main content to calendar
+		calendar.appendChild(this.createCalendar(date));
+	}
 
+	// eslint-disable-next-line class-methods-use-this
+	createCalendar(date) {
+		//# main content
+		const table = document.createElement("table");
+		//## calendar header with arrows
+		const th1 = table.insertRow();
+		th1.id = "cal-header";
+		//### left arrow
+		const leftArrowCell = th1.insertCell();
+		leftArrowCell.id = "left-arrow";
+		leftArrowCell.className = "arrow";
+		leftArrowCell.textContent = "<";
+		leftArrowCell.addEventListener("click", () => {
+			date.setMonth(date.getMonth() - 1);
+			table.remove();
+			this.render(date);
+		});
+		//### display current month and year
+		const displayDay = th1.insertCell();
+		displayDay.id = "display-day";
+		displayDay.colSpan = "5";
+		displayDay.textContent = `${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
+		//### right arrow	
+		const rightArrowCell = th1.insertCell();
+		rightArrowCell.id = "right-arrow";
+		rightArrowCell.className = "arrow";
+		rightArrowCell.textContent = ">";
+		rightArrowCell.addEventListener("click", () => {
+			date.setMonth(date.getMonth() + 1);
+			table.remove();
+			this.render(date);
+		});
+		//## week days row
+		const th2 = table.insertRow();
+		th2.id = "weekday-row";
+		const weeksDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		for (const i of weeksDay) {
+			const tr = th2.insertCell();
+			tr.textContent = i;
+		}
+		//## set day on month
+		const startDay = new Date(date.getFullYear(), date.getMonth(), 1);
+		const movDay = new Date(startDay.getTime());
+		movDay.setDate(-startDay.getDay() + 1);
+		console.log(movDay);
+		const curMonth = startDay.getMonth();
+		// eslint-disable-next-line no-constant-condition
+		while (true) {
+			const tr = table.insertRow();
+			for (let i = 0; i < 7; i++) {
+				const cell = tr.insertCell();
+				cell.textContent = movDay.getDate();
+				if (movDay.getDay() === i) {
+					if (movDay.getMonth() === curMonth) {
+						cell.className = "cur-month month-cell";
+						const obj = {
+							month: (movDay.getMonth() + 1).toString().padStart(2, '0'),
+							day: (cell.textContent).padStart(2, '0'),
+							year: movDay.getFullYear()
+						};
+						cell.addEventListener("click", () => this.callback(this.id, obj));
+						if (movDay.getDate() === this.date.getDate() && curMonth === this.date.getMonth()) cell.id = "cur-day";
+					} else {
+						cell.className = "other-month month-cell";
+					}
+					movDay.setDate(movDay.getDate() + 1);
+				} else {
+					cell.className = "other-month month-cell";
+				}
+			}
+			if (movDay.getMonth() !== curMonth) break;
+		}
+		return table;
+	}
+}
